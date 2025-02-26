9a1e0757d056ce2ceb4e8f2799e2a622adcd7394
Abdulrahman ALfaouri <abdulrahmanalfaouri@gmail.com>
2025-02-26 13:38:02 +0300

    Update main.py
    code refactored

--- 2.py
+++ 2.py
@@ -3,7 +3,6 @@ from os.path import isfile, join
 import spacy
 
 
-
 textfiles = [f for f in listdir('F:\\textFiles') if isfile(join('F:\\textFiles', f))]
 nlp = spacy.load("en_core_web_md")
 
@@ -13,15 +12,12 @@ def Calculate_Similarity(text1, text2):
     return base.similarity(compare)
 
 t1 = input("Enter a word or a sentence to find the clip for it :")
-# input = Cover your butt.
 for textFile in textfiles:
     with open(f'F:\\textFiles\\{textFile}') as txtfile:
         t2 = txtfile.readlines()[1]
         simularity = round(Calculate_Similarity(t1,t2),1)
         if simularity == (1.0 or 0.9 or 0.8):
-            print(simularity)
-            print(t2)
-            # print(f'F:\\textFiles\\{textFile}')
+            print(simularity, t2)
             txtFileNumber = textFile[4]
             print(f"F:\\VidoeClips\\Outout{txtFileNumber}.mp4")
             break
