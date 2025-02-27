466a3289c2d0902d9637c2c1cac6238f4782cea4
Abdulrahman ALfaouri <abdulrahmanalfaouri@gmail.com>
2025-02-27 14:47:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,13 +11,16 @@ def Calculate_Similarity(text1, text2):
     compare = nlp(text2)
     return base.similarity(compare)
 
-t1 = input("Enter a word or a sentence to find the clip for it :")
+WantedSentance = input("Enter a word or a sentence to find the clip for it :")
+
 for textFile in textfiles:
     with open(f'F:\\textFiles\\{textFile}') as txtfile:
-        t2 = txtfile.readlines()[1]
-        simularity = round(Calculate_Similarity(t1,t2),1)
+        SentanceFromTheSubs = txtfile.readlines()[1]
+        
+        simularity = round(Calculate_Similarity(WantedSentance, SentanceFromTheSubs),1)
+        
         if simularity == (1.0 or 0.9 or 0.8):
-            print(simularity, t2)
+            print(simularity, SentanceFromTheSubs)
             txtFileNumber = textFile[4]
             print(f"F:\\VidoeClips\\Outout{txtFileNumber}.mp4")
             break
